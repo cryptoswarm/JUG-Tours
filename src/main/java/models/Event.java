c686471646f7230c73534dce8caaa1bbddf8d4b4
cryptoswarm <mokhtar.safir@outlook.com>
2022-09-21 15:24:22 -0400

    creating models user, group and event

---
+++ src/main/java/models/Event.java
@@ -0,0 +1,28 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private Instant date;
+    private String title;
+    private String description;
+
+    @ManyToMany
+    private Set<User> attendees;
+}
